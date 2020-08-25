a4b199603e9cddc93c5d14cacbc907df306d6459
ya.ileyka <ya.ileyka@ya.ru>
2020-08-25 22:35:52 +0400

    Simple server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
-//hello world
-console.log("Hello? world!!")
+const http = require("http")
+const hostname = "127.0.0.1"
+const port = 10207
+const server = http.createServer((req, res) => {
+	res.statusCode = 200
+	res.setHeader("Content-Type", "text/plain")
+	res.end("Hello? world!!\n")
+})
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}`)
+})
