578830992561e35039f2007b7870ad1ca07f918e
ya.ileyka <ya.ileyka@ya.ru>
2020-08-27 16:08:17 +0400

    fixed broken response string

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 const http = require("http")
 const hostname = "127.0.0.1"
 const port = 10207
+let   strRes = "Hello, world!! I`am server NodeJS\n"
 const server = http.createServer((req, res) => {
 	res.statusCode = 200
 	res.setHeader("Content-Type", "text/plain")
-	res.end("Hello? world!!\n")
+	res.end(strRes)
 })
 
 server.listen(port, hostname, () => {
